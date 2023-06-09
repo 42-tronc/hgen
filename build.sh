c439c49a3377cc3bcc7195f69dec10df2e6b2cc9
maplepy <github.register@slmail.me>
2023-06-09 18:03:24 +0200

    fix shellcheck

--- build.sh
+++ build.sh
@@ -5,44 +5,45 @@ TYPE=$1
 
 # Function to clean up the 'dist' directory
 cleanup() {
-  rm dist/* 2> /dev/null
+	rm dist/* 2> /dev/null
 }
 
 # Function to update the version using semver.py script
 setup_semver() {
-  TYPE=$1
-  python3 semver.py $TYPE
+	TYPE=$1
+	python3 semver.py "$TYPE"
 }
 
 # Function to build the package
 build() {
-  python3 -m build
+	python3 -m build
 }
 
 # Function to upload the package to the test PyPI repository
 upload() {
-  python3 -m twine upload --repository testpypi dist/*
+	python3 -m twine upload dist/*
+	# python3 -m twine upload --repository testpypi dist/*
 }
 
 # Function to refresh dependencies and push the changes to the git repository
 refresh() {
-  sleep 1
-  pip3 install --upgrade hgen
-  asdf reshim
+	sleep 1
+	pip3 install --upgrade genheader
+	asdf reshim
 }
 
 # Function to commit and tag the release version, then push to the git repository
 push() {
-  git commit -am "release@$VER"
-  git tag -a $VER -m "release@$VER"
-  git push
+	git commit -am "release@$VER"
+	git tag -a "$VER" -m "release@$VER"
+	git push
 }
 
 # Clean up the 'dist' directory
 cleanup
 
 # Set the version using semver.py script
-VER="v$(setup_semver $TYPE)"
+VER="v$(setup_semver "$TYPE")"
 
 # Build the package
 build
